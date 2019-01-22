a92b47fec4a2246fd598128d054426a17897852a
Gandjurov <gandjurov@abv.bg>
2019-01-22 15:45:51 +0200

    Add Books After Single Responsibility Principle

---
+++ 01.SOLID/SOLID Principles/02. After - Books/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleResponsibilityBooksAfter
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string TurnPage(int page)
+        {
+            return "Current page";
+        }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/02. After - Books/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace SingleResponsibilityBooksAfter
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
