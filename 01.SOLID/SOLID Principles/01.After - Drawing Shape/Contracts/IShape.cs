52038f246723a293fcd9285ea1f53c46c9331985
Gandjurov <gandjurov@abv.bg>
2019-01-22 14:58:00 +0200

    Add first Example

---
+++ 01.SOLID/SOLID Principles/01.After - Drawing Shape/Contracts/IShape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleResponsibilityShapesAfter.Contracts
+{
+    public interface IShape
+    {
+        decimal Area { get; }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/01.After - Drawing Shape/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace SingleResponsibilityShapesAfter
+{
+    using SingleResponsibilityShapesAfter.Contracts;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            decimal width = decimal.Parse(Console.ReadLine());
+            decimal height = decimal.Parse(Console.ReadLine());
+
+            IShape rectangleArea = new Rectangle(width, height);
+            Console.WriteLine(rectangleArea.Area);
+        }
+    }
+}
