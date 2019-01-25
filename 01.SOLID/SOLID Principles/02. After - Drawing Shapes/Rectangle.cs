cbe2257eb9c78d43931721b92407b7aabf0cc52e
Gandjurov <gandjurov@abv.bg>
2019-01-25 13:11:14 +0200

    Open/Close Principle

---
+++ 01.SOLID/SOLID Principles/02. After - Drawing Shapes/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿using OpenClosedDrawingShapesAfter.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedDrawingShapesAfter
+{
+    public class Rectangle : IShape
+    {
+    }
+}
