29e0ae22dfe1f9b77f575b036c4a0199328827ba
Gandjurov <gandjurov@abv.bg>
2019-02-01 12:51:15 +0200

    Liskov Substitution Principle

---
+++ 01.SOLID/SOLID Principles/02. After - Employees (LSP)/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovSubstitutionEmployeesAfter
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + this.Name;
+        }
+    }
+}
