569fadc761a8ea658acb52acb318cecae2cb23fe
Gandjurov <gandjurov@abv.bg>
2019-02-01 12:58:12 +0200

    Liskov Substitution Principle

---
+++ 01.SOLID/SOLID Principles/03. Before - Movement (LSP)/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LiskovSubstitutionMovementBefore
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/03. Before - Movement (LSP)/TranslatableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovSubstitutionMovementBefore
+{
+    public class TranslatableObject : MovableObject
+    {
+        public override void Translate()
+        {
+            // do translate logic
+        }
+
+        public override void Rotate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            this.Translate();
+        }
+    }
+}
