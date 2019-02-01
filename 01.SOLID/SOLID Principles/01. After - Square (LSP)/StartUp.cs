9e96122f7324f73e3fb1d2042186ebe2b45c7ccc
Gandjurov <gandjurov@abv.bg>
2019-02-01 11:12:09 +0200

    Liskov Substitution Principle

---
+++ 01.SOLID/SOLID Principles/01. After - Square (LSP)/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LiskovSubstitutionSquareAfter
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/01. Before - Square (LSP)/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovSubstitutionSquareBefore
+{
+    public class Rectangle : Shape
+    {
+        public virtual decimal Width { get; set; }
+
+        public virtual decimal Height { get; set; }
+
+        public override decimal Area
+        {
+            get
+            {
+                return this.Width * this.Height;
+            }
+        }
+    }
+}
