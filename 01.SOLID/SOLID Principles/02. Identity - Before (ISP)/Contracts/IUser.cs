659a1f21429f97eef07bf2871714709f393d032e
Gandjurov <gandjurov@abv.bg>
2019-02-01 17:50:51 +0200

    Interface Segregation Principle

---
+++ 01.SOLID/SOLID Principles/02. Identity - Before (ISP)/Contracts/IUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSegregationIdentityBefore.Contracts
+{
+    public class IUser
+    {
+        string Email { get; }
+
+        string PasswordHash { get; }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/02. Identity - Before (ISP)/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InterfaceSegregationIdentityBefore
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
