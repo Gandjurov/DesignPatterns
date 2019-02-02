9b643a3b1fc4f9f39e0f1477aa56610455f9d6e8
Gandjurov <gandjurov@abv.bg>
2019-02-02 13:42:05 +0200

    Open Close Principle

--- 01.SOLID/SOLID Principles/02. Before - Drawing Shapes (OCP)/Rectangle.cs
+++ 01.SOLID/SOLID Principles/02. Before - Drawing Shapes (OCP)/Rectangle.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenClosedDrawingShapesBefore
+﻿namespace OpenClosedDrawingShapesBefore
 {
+    using OpenClosedDrawingShapesBefore.Contracts;
+
     public class Rectangle : IShape
     {
     }
