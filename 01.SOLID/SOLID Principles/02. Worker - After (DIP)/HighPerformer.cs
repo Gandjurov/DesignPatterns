5723cb2392ddccee1ed2436f02530ce981b7cea8
Gandjurov <gandjurov@abv.bg>
2019-02-05 14:58:47 +0200

    Dependency Inversion Principle

---
+++ 01.SOLID/SOLID Principles/02. Worker - After (DIP)/HighPerformer.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInversionWorkerAfter.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInversionWorkerAfter
+{
+    public class HighPerformer : IWorker
+    {
+        public void Work()
+        {
+            // do the work faster
+        }
+    }
+}
---
+++ 01.SOLID/SOLID Principles/02. Worker - After (DIP)/Worker.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInversionWorkerAfter.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInversionWorkerAfter
+{
+    public class Worker : IWorker
+    {
+        public void Work()
+        {
+            // do the work
+        }
+    }
+}
