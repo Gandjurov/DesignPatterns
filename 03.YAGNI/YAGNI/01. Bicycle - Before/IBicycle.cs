a8bc1ccc7d117bd49015b68a5ab996b544d79219
Gandjurov <gandjurov@abv.bg>
2019-05-01 13:50:12 +0300

    Add YAGNI example: 01. Bicycle - Before

---
+++ 03.YAGNI/YAGNI/01. Bicycle - Before/IBicycle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YAGNIBicycleBefore
+{
+    public interface IBicycle
+    {
+        int Wheels { get; }
+
+        int Seat { get; }
+
+        string Body { get; }
+
+        int Breaks { get; }
+
+        string Chain { get; }
+
+        string Steering { get; }
+
+        bool IsElectric { get; }
+
+        string Engine { get; }
+
+        string TypeOfFuel { get; }
+
+        int AssistingWheels { get; }
+
+        string Umbrella { get; }
+
+        string Gps { get; }
+
+        int Mirrors { get; }
+
+        int Stickers { get; }
+
+        int LuggageVolume { get; }
+    }
+}
---
+++ 03.YAGNI/YAGNI/01. Bicycle - Before/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAGNIBicycleBefore
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
