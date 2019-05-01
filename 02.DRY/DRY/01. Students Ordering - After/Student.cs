874ecf88286941072e30fe8b6ec24c81124a26c5
Gandjurov <gandjurov@abv.bg>
2019-05-01 13:06:04 +0300

    Add After example

---
+++ 02.DRY/DRY/01. Students Ordering - After/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DRYStudentsOrderingAfter
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public double Mark { get; set; }
+
+        public int Participations { get; set; }
+    }
+}
