fe99ca1d52f01d72870d7580812f92f081948f6f
Gandjurov <gandjurov@abv.bg>
2019-05-01 14:21:28 +0300

    Add KISS example: 01. MP3 Mover - After

---
+++ 04.KISS/KISS/01. MP3 Mover - After/Mp3Mover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace KISSMp3MoverAfter
+{
+    public class Mp3Mover
+    {
+        public static void Main(string[] args)
+        {
+            const string DirectoryPath = @"C:\MyMusic\";
+
+            foreach (var fileName in Directory.EnumerateFiles(DirectoryPath))
+            {
+                if (fileName.IndexOf(" - ") >= 0 && fileName.ToLower().EndsWith("mp3"))
+                {
+                    File.Move(fileName, fileName.Substring(fileName.IndexOf(" - ") + 3));
+                    var artist = fileName.Substring(0, fileName.IndexOf(" - "));
+                    File.Move(fileName, artist + "/" + fileName);
+                }
+            }
+        }
+    }
+}
