a38e8173b1797c296981facaf5a594e4595e5919
Gandjurov <gandjurov@abv.bg>
2019-05-01 13:52:40 +0300

    Add YAGNI example: 01. Bicycle - After

---
+++ 03.YAGNI/YAGNI/01. Bicycle - After/IBicycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YAGNIBicycleAfter
+{
+    public interface IBicycle
+    {
+        int Wheels { get; }
+
+        int Seat { get; }
+
+        string Body { get; }
+
+        int Breaks { get; }
+
+        string Chain { get; }
+
+        string Steering { get; }
+    }
+}
---
+++ 03.YAGNI/YAGNI/01. Bicycle - After/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAGNIBicycleAfter
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
